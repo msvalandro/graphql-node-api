e3f0181fc8383c575d64d582f9d6c3f2ce2dc3d6
msvalandro <msvalandro@gmail.com>
2018-07-22 17:49:44 -0300

    Implementation of resolvers.

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,9 +1,19 @@
 import { makeExecutableSchema } from 'graphql-tools';
+import { merge } from 'lodash';
 import { Query } from './query';
 import { Mutation } from './mutation';
 import { userTypes } from './resources/user/user.schema';
 import { postTypes } from './resources/post/post.schema';
 import { commentTypes } from './resources/comment/comment.schema';
+import { userResolvers } from './resources/user/user.resolvers';
+import { postResolvers } from './resources/post/post.resolvers';
+import { commentResolvers } from './resources/comment/comment.resolvers';
+
+const resolvers = merge(
+	userResolvers,
+	postResolvers,
+	commentResolvers
+);
 
 const SchemaDefinition = `
 	type Schema {
@@ -20,5 +30,6 @@ export default makeExecutableSchema({
 		userTypes,
 		postTypes,
 		commentTypes
-	]
+	],
+	resolvers
 });
